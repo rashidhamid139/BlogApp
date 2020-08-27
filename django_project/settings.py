2348462bf98e3dafc57b2ae863ddd224274ae7de
Rashid Hamid Dar <rashidhamid139@gmail.com>
2020-08-27 23:41:32 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -183,7 +183,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'rashidhamid139@gmail.com'
-EMAIL_HOST_PASSWORD = 'bhjmbwqvhuzsdxaf'
+EMAIL_HOST_PASSWORD = ''
 
 DEFAULT_FROM_USER = EMAIL_HOST_USER
 
